4b61bdc7de230d684322154ec7e874054eb2c598
frederiksamra <117893539+frederiksamra@users.noreply.github.com>
2022-11-17 16:22:04 +0700

    Add files via upload

---
+++ 1_C_71220913.py
@@ -0,0 +1,19 @@
+namadepan = 'Cassandra'
+namabelakang = 'Charlotte'
+matakuliah = "logika matematika"
+grup = 'a'
+input("Masukkan nama Anda: Cassandra Charlotte")
+input("Masukkan mata kuliah: Logika Matematika")
+input("Masukkan grup: A")
+print('hallo',namabelakang,namadepan)
+print(namabelakang,"tergabung dalam kelas",matakuliah,"pada grup",grup)
+
+namadepan = 'Alaska Sebastian'
+namabelakang = 'Dirgantara'
+matakuliah = "Teknologi Komputer"
+grup = 'D'
+input("Masukkan nama Anda: Alaska Sebastian Dirgantara")
+input("Masukkan mata kuliah: Teknologi Komputer")
+input("Masukkan grup: D")
+print('hallo',namabelakang,namadepan)
+print(namabelakang,"tergabung dalam kelas",matakuliah,"pada grup",grup)
---
+++ 2_C_71220913.py
@@ -0,0 +1,17 @@
+kecepatan = int(input("Masukkan kecepatan tempuh"))
+waktu = int(input("Masukkan waktu yang diperlukan"))
+#rumus
+bensin = kecepatan*waktu//10
+biaya = bensin*15000
+print("teman anda mengisi bensin sebanuak",bensin,"liter")
+print("biaya yang dikeluarkan untuk mengisi bensin adalah",biaya)
+
+kecepatan = int(input("Masukkan kecepatan tempuh"))
+waktu = int(input("Masukkan waktu yang diperlukan"))
+#rumus
+bensin = kecepatan*waktu//10
+biaya = bensin*15000
+print("teman anda mengisi bensin sebanuak",bensin,"liter")
+print("biaya yang dikeluarkan untuk mengisi bensin adalah",biaya)
+
+
